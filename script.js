8e3399fa722d46175ff39527cc71c85b3ab6c3a3
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-06-15 16:36:37 +0300

    Refactor code

--- script.js
+++ script.js
@@ -11,52 +11,58 @@ function getComputerChoice() {
 
 // Write the logic to get the human choice
 function getHumanChoice() {
-  let userInput = prompt("Please choose between rock, paper, and scissors.").toLocaleLowerCase();
-  if (userInput !== "rock" && userInput !== "paper" && userInput !== "scissors") {
+  let humanChoice = prompt("Please choose between rock, paper, and scissors.").toLocaleLowerCase();
+  if (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
     alert("Invalid input!");
-    userInput = prompt("Please choose between rock, paper, and scissors.").toLocaleLowerCase();
+    humanChoice = prompt("Please choose between rock, paper, and scissors.").toLocaleLowerCase();
   } 
-  return userInput;
+  return humanChoice;
 }
-
-// Declare the players score variables
-let computerScore = 0;
-let humanScore = 0;
 
 // Write the logic to play a single round
 function playRound(humanChoice, computerChoice) {
   humanChoice = getHumanChoice();
-  console.log("Your choice:", humanChoice)
+  console.log("Your choice:", humanChoice);
   computerChoice = getComputerChoice();
-  console.log("Computer's choice:", computerChoice)
-
+  console.log("Computer's choice:", computerChoice);
+  
+  // Declare the players score variables
+  let computerScore = 0;
+  let humanScore = 0;
+  
     //COM win scenarios
-  if ((computerChoice === 'paper' && humanChoice === 'rock') || (computerChoice === 'rock' && humanChoice === 'scissors') || (computerChoice === 'scissors' && humanChoice === 'paper')) {
+  if (
+    (computerChoice === 'paper' && humanChoice === 'rock') || 
+    (computerChoice === 'rock' && humanChoice === 'scissors') || 
+    (computerChoice === 'scissors' && humanChoice === 'paper')
+  ) {
     computerScore++;
-    console.log(`“You lose! ${computerChoice} beats ${humanChoice}”`)
+    console.log(`“You lose! ${computerChoice} beats ${humanChoice}”`);
 
     //Human win scenarios
-  } else if ((computerChoice === 'rock' && humanChoice === 'paper') || (computerChoice === 'scissors' && humanChoice === 'rock') || (computerChoice === 'paper' && humanChoice === 'scissors')){
+  } else if (
+    (computerChoice === 'rock' && humanChoice === 'paper') || 
+    (computerChoice === 'scissors' && humanChoice === 'rock') || 
+    (computerChoice === 'paper' && humanChoice === 'scissors')
+  ) {
     humanScore++;
-    console.log('“You win! ' + humanChoice + ' beats ' + computerChoice + '”')
+    console.log(`“You win! ${humanChoice} beats ${computerChoice}”`);
   } else {
-    console.log("It's a draw!")
+    console.log("It's a draw!");
   }
 }
 
 // Write the logic to play the entire game
-function playGame() {
+(function playGame() {
   for (let i = 1; i <= 5; i++) {
     playRound();
-    console.log("Scores:", computerScore, humanScore)
+    console.log("Scores:", computerScore, humanScore);
   }
   if (computerScore > humanScore) {
-    console.log("Sorry, you lost the game! :'(")
+    console.log("Sorry, you lost the game! :'(");
   } else if (humanScore > computerScore) {
     console.log("Congrats! You won :D")
   } else {
-    console.log("We don't have a winner :(")
+    console.log("We don't have a winner :(");
   }
-}
-
-playGame();
+})();
