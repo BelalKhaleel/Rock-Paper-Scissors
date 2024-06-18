28643f7bdc6fdb52ff52fc99e49cc4e0f879f49e
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-06-18 08:40:04 +0300

    Change computer choice gesture according to result

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let computerScore = 0;
+let humanScore = 0;
+
+let humanChoices = document.querySelector(".human-choices");
+humanChoices.addEventListener("click", playRound);
+
+let svgs = document.querySelectorAll("svg");
+
 function getComputerChoice() {
   if (Math.random() < 0.3) {
     return "rock";
@@ -8,77 +16,97 @@ function getComputerChoice() {
   }
 }
 
-let computerScore = 0;
-let humanScore = 0;
-
-let humanChoices = document.querySelector('.human-choices');
-humanChoices.addEventListener('click', playRound);
-
-let svgs = document.querySelectorAll('svg');
-      
-  function playRound(e) {
-    // debugger
-
-    svgs.forEach(svg => {
-      svg.classList.remove('red');
-      svg.classList.remove('green');
-      svg.classList.remove('blue');
-      svg.classList.add('black');
-    });
-
-    let humanChoice = "";
-    console.log(e.target);
-    if (e.target.closest('.rock')) {
-      humanChoice = 'rock';
-    } else if (e.target.closest('.paper')) {
-      humanChoice = 'paper';
-    } else if (e.target.closest('.scissors')){
-      humanChoice = 'scissors';
-    }
-
-    let computerChoice = getComputerChoice();
-
-    const roundResult = document.querySelector('.round-result');
-
-    const playerScore = document.querySelector('.player-score');
-    const comScore = document.querySelector('.computer-score');
-
-    if (
-      (computerChoice === 'paper' && humanChoice === 'rock') || 
-      (computerChoice === 'rock' && humanChoice === 'scissors') || 
-      (computerChoice === 'scissors' && humanChoice === 'paper')
-    ) {
-      computerScore++;
-      roundResult.textContent = `“You lose! ${computerChoice} beats ${humanChoice}”`;
-      const svg = e.target.closest('svg');
-      svg.classList.remove('black');
-      svg.classList.add('red');
-    } else if (
-      (computerChoice === 'rock' && humanChoice === 'paper') || 
-      (computerChoice === 'scissors' && humanChoice === 'rock') || 
-      (computerChoice === 'paper' && humanChoice === 'scissors')
-    ) {
-      humanScore++;
-      roundResult.textContent = `“You win! ${humanChoice} beats ${computerChoice}”`;
-      const svg = e.target.closest('svg');
-      svg.classList.remove('black');
-      svg.classList.add('green');
-    } else {
-      roundResult.textContent = `“It's a draw! Both chose ${computerChoice}”`;
-      const svg = e.target.closest('svg');
-      svg.classList.remove('black');
-      svg.classList.add('blue');
-    }
-    comScore.textContent = computerScore;
-    playerScore.textContent = humanScore;  
-    
-    const finalResult = document.querySelector('.final-result');
+function playRound(e) {
   
-    if (computerScore === 5) {
-      finalResult.textContent = "Sorry, you lost the game! :'(";
-      humanChoices.removeEventListener('click', playRound)
-    } else if (humanScore === 5) {
-      finalResult.textContent = "Congrats! You won :D";
-      humanChoices.removeEventListener('click', playRound)
+  function colorHumanChoice(e, color) {
+    const svg = e.target.closest("svg");
+    svg.classList.remove("black");
+    svg.classList.add(color);
+  }
+
+  function colorComputerChoice(humanChoice, color) {
+    switch (humanChoice) {
+      case "rock":
+        computerPaper.classList.remove("black");
+        computerPaper.classList.add(color);
+        break;
+      case "paper":
+        computerScissors.classList.remove("black");
+        computerScissors.classList.add(color);
+        break;
+      case "scissors":
+        computerRock.classList.remove("black");
+        computerRock.classList.add(color);
+        break;
     }
   }
+
+  svgs.forEach((svg) => {
+    svg.classList.remove("red");
+    svg.classList.remove("green");
+    svg.classList.remove("blue");
+    svg.classList.add("black");
+  });
+
+  let humanChoice = "";
+  if (e.target.closest(".rock")) {
+    humanChoice = "rock";
+  } else if (e.target.closest(".paper")) {
+    humanChoice = "paper";
+  } else if (e.target.closest(".scissors")) {
+    humanChoice = "scissors";
+  }
+
+  let computerChoice = getComputerChoice();
+
+  const roundResult = document.querySelector(".round-result");
+
+  const playerScore = document.querySelector(".player-score");
+  const comScore = document.querySelector(".computer-score");
+
+  const computerRock = document.getElementById("rock");
+  const computerPaper = document.getElementById("paper");
+  const computerScissors = document.getElementById("scissors");
+
+  const p = document.querySelector("p");
+
+  if (
+    (computerChoice === "paper" && humanChoice === "rock") ||
+    (computerChoice === "rock" && humanChoice === "scissors") ||
+    (computerChoice === "scissors" && humanChoice === "paper")
+  ) {
+    computerScore++;
+    roundResult.textContent = `“You lose! ${computerChoice} beats ${humanChoice}”`;
+    colorHumanChoice(e, "red");
+    colorComputerChoice(humanChoice, "green");
+  } else if (
+    (computerChoice === "rock" && humanChoice === "paper") ||
+    (computerChoice === "scissors" && humanChoice === "rock") ||
+    (computerChoice === "paper" && humanChoice === "scissors")
+  ) {
+    humanScore++;
+    roundResult.textContent = `“You win! ${humanChoice} beats ${computerChoice}”`;
+    colorHumanChoice(e, "green");
+    colorComputerChoice(humanChoice, "red");
+  } else {
+    roundResult.textContent = `“It's a draw! Both chose ${computerChoice}”`;
+    colorHumanChoice(e, "blue");
+    computerPaper.classList.remove("black");
+    computerPaper.classList.add("blue");
+  }
+
+  comScore.textContent = computerScore;
+  playerScore.textContent = humanScore;
+
+  const finalResult = document.querySelector(".final-result");
+
+  if (computerScore === 5) {
+    finalResult.textContent = "Sorry, you lost the game! :'(";
+    p.classList.remove("d-none");
+    humanChoices.removeEventListener("click", playRound);
+  } else if (humanScore === 5) {
+    finalResult.textContent = "Congrats! You won :D";
+    p.classList.remove("d-none");
+    humanChoices.removeEventListener("click", playRound);
+  }
+}
